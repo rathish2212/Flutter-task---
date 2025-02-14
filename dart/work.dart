eaa134a87aca4147efd649f39afa50f36373a65e
RATHISHKUMAR M <94343522+rathish2212@users.noreply.github.com>
2025-02-14 14:48:57 +0530

    Add files via upload

---
+++ dart/work.dart
@@ -0,0 +1,30 @@
+//Problem 1:
+void main() {
+  List<int> arr = [7, 18, 45, -6, 19, -1, 0];
+  int count = 0;
+  for (int num in arr) {
+    if (num < 0) {
+      count++;
+    }
+  }
+  print(" The total count of negative elements: $count");
+}
+
+// output:
+// The total count of negative elements: 2
+
+
+
+//problem 2
+void main() {
+  
+  String str = "flesruoYevileB";
+  String reverse = "";
+  for (int i = str.length - 1; i >= 0; i--) {
+    reverse += str[i];
+  }
+  print(' The reversed String is: $reverse');
+}
+
+//output:
+//The reversed string is:BeliveYourself
